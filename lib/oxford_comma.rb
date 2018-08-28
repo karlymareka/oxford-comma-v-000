55c52bf763e01c7cf538efe6fcffb720a564e581
Karly Mareka <karly.mareka@gmail.com>
2018-08-28 17:00:37 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
     array.join(" and ")
   else
     last_item = array.pop 
-    array.join(",")
+    string = array.join(",")
     return array + ", and #{last_item}"
   end 
   
